48fb61641a796281df47ca2b533c71b2ac06eab0
Maciej Anielski <maciej@anielski.co.uk>
2025-06-07 21:55:26 +0100

    Cleaned Trump Polling Dates.

---
+++ trump_polling.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import requests
+from io import StringIO
+
+from datetime import datetime
+
+# Define Functions
+
+def split_date_range(df, column_name, start_col = 'date_start', end_col = 'date_end'):
+
+    def parse_date_range(date_string):
+
+        try:
+
+            clean_string = date_string.split('@@')[0].strip()
+
+            parts = clean_string.split(',')
+
+            date_range = parts[0].strip()
+
+            year = parts[1].strip()
+
+            start_date_str, end_date_str = date_range.split(' - ')
+
+            start_date_full = f"{start_date_str}/{year}"
+            end_date_full = f"{end_date_str}/{year}"
+
+            start_date = pd.to_datetime(start_date_full, format = '%m/%d/%Y')
+            end_date =  pd.to_datetime(end_date_full, format = '%m/%d/%Y')
+
+            return start_date, end_date
+
+        except Exception as e:
+
+            print(f"Error parsing date: {date_string}, Error {e}")
+
+            return(pd.NaT, pd.NaT)
+
+    df_copy = df.copy()
+
+    date_pairs = df_copy[column_name].apply(parse_date_range)
+
+    df_copy[start_col] = [pair[0] for pair in date_pairs]
+
+    df_copy[end_col] = [pair[1] for pair in date_pairs]
+
+    return df_copy
+            
+# Read Trump Approval Rating .csv from URL
+
+url = "https://static.dwcdn.net/data/wWI2Y.csv?v=1749325560000"
+
+response = requests.get(url)
+response.raise_for_status()
+
+df = pd.read_csv(StringIO(response.text))
+
+# Clean Up Dates
+
+df_clean = split_date_range(df, 'Dates')
+
+print(df_clean[['date_start','date_end']])
