c6770cb2c424646e25f749d170fe2c77f77c02d7
Maciej Anielski <maciej@anielski.co.uk>
2025-06-10 17:43:55 +0100

    Added Two Circles to Comparing Maginutudes Code.

--- comparing_magnitudes/comparing_magnitudes_tool.py
+++ comparing_magnitudes/comparing_magnitudes_tool.py
@@ -1,14 +1,30 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-theta = np.linspace(0, 2 * np.pi, 100)
+area1 = 0.6
+area2 = 193
 
-radius = 1
+radius1 = (area1 / np.pi) ** 0.5
+radius2 = (area2 / np.pi) ** 0.5
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+theta1 = np.linspace(0, 2 * radius1 * np.pi, 100)
+theta2 = np.linspace(0, 2 * radius2 * np.pi, 100)
 
-plt.figure(figsize = (8,8))
-plt.plot(x, y, 'b-', linewidth = 2, label = 'Circle (r=1)')
-plt.axis('equal')
+x1 = radius1 * np.cos(theta1)
+y1 = radius1 * np.sin(theta1)
+
+x2 = radius2 * np.cos(theta2)
+y2 = radius2 * np.sin(theta2)
+
+fig, axes = plt.subplots(1, 2, figsize = (10, 5))
+
+axes[0].plot(x1, y1)
+axes[0].set_title('Value 1')
+
+axes[1].plot(x2, y2)
+axes[1].set_title('Value 2')
+
+plt.tight_layout()
+
 plt.savefig('circle.png', dpi = 300)
+
